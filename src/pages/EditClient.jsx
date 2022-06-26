46889854009716895398d823226ad863900a6cec
David <davidnavarretea1996@gmail.com>
2022-06-26 13:04:10 +0200

    First commit

---
+++ src/pages/EditClient.jsx
@@ -0,0 +1,9 @@
+const EditClient = () => {
+  return (
+    <div>
+        <h1>EditClient</h1>
+    </div>
+  )
+}
+
+export default EditClient
---
+++ src/pages/NewClient.jsx
@@ -0,0 +1,9 @@
+const NewClient = () => {
+  return (
+    <div>
+        <h1>NewClient</h1>
+    </div>
+  )
+}
+
+export default NewClient
