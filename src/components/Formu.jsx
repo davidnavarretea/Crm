54e0076fc1ceeebfc471dd29ff32c2f9a47b7b17
David <davidnavarretea1996@gmail.com>
2022-06-26 22:04:10 +0200

    Finish day

--- src/components/Formu.jsx
+++ src/components/Formu.jsx
@@ -29,7 +29,7 @@ const Formu = () => {
   }
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto">
-        <h1 className="text-gray-600 font-bold text-xl uppercase text-center">Add Client</h1>
+        <h1 className="text-blue-900 font-bold text-xl uppercase text-center">Add Client</h1>
         <Formik
           initialValues={{
             name: '',
