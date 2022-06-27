7cb391424aeb171d3845810f0f165cafce7702ce
David <davidnavarretea1996@gmail.com>
2022-06-27 23:09:35 +0200

    Alpha version

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,54 +39,57 @@ const Home = () => {
   }
   return (
     <>
-      {loading ?
+      {deleting ? (
         <div className="flex justify-center items-center h-screen">
-            <div className="sk-chase">
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-            </div>
-        </div> : (
-            <>
-                <h1 className="font-black text-4xl text-blue-900">Clients</h1>
-                <p className="mt-3 text-gray-600 font-bold">Manage your clients</p>
-                <table className="w-full mt-5 table-auto bg-white shadow">
-                  <thead className="bg-blue-900 text-white">
-                    <tr className="shadow-lg">
-                      <th className="p-2">Name</th>
-                      <th className="p-2">Business</th>
-                      <th className="p-2">Email</th>
-                      <th className="p-2">Phone</th>
-                      <th className="p-2">Actions</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {clients.map(client => (
-                      <Client
-                        key={client.id}
-                        client={client}
-                        handleDelete={handleDelete}
-                      />
-                    ))}
-                  </tbody>
-                </table>
-                {deleting && (
-                  <div className="flex justify-center items-center h-20">
-                    <div className="sk-chase">
-                        <div className="sk-chase-dot"></div>
-                        <div className="sk-chase-dot"></div>
-                        <div className="sk-chase-dot"></div>
-                        <div className="sk-chase-dot"></div>
-                        <div className="sk-chase-dot"></div>
-                        <div className="sk-chase-dot"></div>
-                    </div>
-                  </div>
-                )}
-            </>
-        )}
+          <div className="sk-chase">
+              <div className="sk-chase-dot"></div>
+              <div className="sk-chase-dot"></div>
+              <div className="sk-chase-dot"></div>
+              <div className="sk-chase-dot"></div>
+              <div className="sk-chase-dot"></div>
+              <div className="sk-chase-dot"></div>
+          </div>
+        </div>
+      ) : (
+        <>
+          {loading ?
+            <div className="flex justify-center items-center h-screen">
+                <div className="sk-chase">
+                    <div className="sk-chase-dot"></div>
+                    <div className="sk-chase-dot"></div>
+                    <div className="sk-chase-dot"></div>
+                    <div className="sk-chase-dot"></div>
+                    <div className="sk-chase-dot"></div>
+                    <div className="sk-chase-dot"></div>
+                </div>
+            </div> : (
+              <>
+                  <h1 className="font-black text-4xl text-blue-900">Clients</h1>
+                  <p className="mt-3 text-gray-600 font-bold">Manage your clients</p>
+                  <table className="w-full mt-5 table-auto bg-white shadow">
+                    <thead className="bg-blue-900 text-white">
+                      <tr className="shadow-lg">
+                        <th className="p-2">Name</th>
+                        <th className="p-2">Business</th>
+                        <th className="p-2">Email</th>
+                        <th className="p-2">Phone</th>
+                        <th className="p-2">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {clients.map(client => (
+                        <Client
+                          key={client.id}
+                          client={client}
+                          handleDelete={handleDelete}
+                        />
+                      ))}
+                    </tbody>
+                  </table>
+              </>
+            )}
+        </>
+      )}
     </>
   )
 }
--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -5,21 +5,24 @@ const NewClient = () => {
   const [adding, setAdding] = useState(false)
   return (
     <>
-        <h1 className="font-black text-4xl text-blue-900">New Client</h1>
-        <p className="mt-3 text-gray-600 font-bold">Fill out next fields for register new client</p>
-        <Formu setAdding={setAdding}/>
-        {adding && (
-          <div className="flex justify-center items-center h-20">
-            <div className="sk-chase">
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
+      {adding ? (
+            <div className="flex justify-center items-center h-screen">
+              <div className="sk-chase">
+                  <div className="sk-chase-dot"></div>
+                  <div className="sk-chase-dot"></div>
+                  <div className="sk-chase-dot"></div>
+                  <div className="sk-chase-dot"></div>
+                  <div className="sk-chase-dot"></div>
+                  <div className="sk-chase-dot"></div>
+              </div>
             </div>
-          </div>
-        )}
+          ) : (
+            <>
+              <h1 className="font-black text-4xl text-blue-900">New Client</h1>
+              <p className="mt-3 text-gray-600 font-bold">Fill out next fields for register new client</p>
+              <Formu setAdding={setAdding}/>
+            </>
+          )}
     </>
   )
 }
