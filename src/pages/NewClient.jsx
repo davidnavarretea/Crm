516998c9e0ba503c3cadc8d82011cc285d393540
David <davidnavarretea1996@gmail.com>
2022-06-27 18:28:26 +0200

    Latest spinners

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react"
 import Formu from "../components/Formu"
+import '../styles/Spinner.css'
 const NewClient = () => {
+  const [adding, setAdding] = useState(false)
   return (
     <>
         <h1 className="font-black text-4xl text-blue-900">New Client</h1>
         <p className="mt-3 text-gray-600 font-bold">Fill out next fields for register new client</p>
-        <Formu/>
+        <Formu setAdding={setAdding}/>
+        {adding && (
+          <div className="flex justify-center items-center h-20">
+            <div className="sk-chase">
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+            </div>
+          </div>
+        )}
     </>
   )
 }
