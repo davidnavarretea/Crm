704f92ebce9b26dd81c848773aa3cf305024b396
David <davidnavarretea1996@gmail.com>
2022-06-28 02:11:31 +0200

    Finish

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import Client from "../components/Client"
 import '../styles/Spinner.css'
+import animate from '../img/1.gif'
 const Home = () => {
   const [clients, setClients] = useState([])
   const [loading, setLoading] = useState(true)
@@ -37,6 +38,7 @@ const Home = () => {
       }
     }
   }
+  const userScreen = document.documentElement.clientWidth
   return (
     <>
       {deleting ? (
@@ -66,30 +68,36 @@ const Home = () => {
               Object.keys(clients).length === 0 ? (
                 <h1 className="font-black text-4xl text-blue-900">No Clients Yet</h1>
               ) : (
-                <>
-                  <h1 className="font-black text-4xl text-blue-900">Clients</h1>
-                  <p className="mt-3 text-gray-600 font-bold">Manage your clients</p>
-                  <table className="w-full mt-5 table-auto bg-white shadow">
-                    <thead className="bg-blue-900 text-white">
-                      <tr className="shadow-lg">
-                        <th className="p-2">Name</th>
-                        <th className="p-2">Business</th>
-                        <th className="p-2">Email</th>
-                        <th className="p-2">Phone</th>
-                        <th className="p-2">Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {clients.map(client => (
-                        <Client
-                          key={client.id}
-                          client={client}
-                          handleDelete={handleDelete}
-                        />
-                      ))}
-                    </tbody>
-                  </table>
-                </>
+                userScreen < 700 ? (
+                  <div className="h-full flex justify-center">
+                    <img className="w-[50%]" src={animate} alt='Rotate'/>
+                  </div>
+                ) : (
+                  <>
+                    <h1 className="font-black text-4xl text-blue-900">Clients</h1>
+                    <p className="mt-3 text-gray-600 font-bold">Manage your clients</p>
+                    <table className="w-full mt-5 table-auto bg-white shadow">
+                      <thead className="bg-blue-900 text-white">
+                        <tr className="shadow-lg">
+                          <th className="p-2">Name</th>
+                          <th className="p-2">Business</th>
+                          <th className="p-2">Email</th>
+                          <th className="p-2">Phone</th>
+                          <th className="p-2">Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {clients.map(client => (
+                          <Client
+                            key={client.id}
+                            client={client}
+                            handleDelete={handleDelete}
+                          />
+                        ))}
+                      </tbody>
+                    </table>
+                  </>
+                )
               )
             )}
         </>
